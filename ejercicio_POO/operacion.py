bdd6d51ff50ba9a7dc30aeb18f0147ed6ff093c5
Jorge González <98619897+JorchZombie@users.noreply.github.com>
2025-05-20 23:09:35 -0600

    Delete ejercicio_POO directory

--- ejercicio_POO/operacion.py
+++
@@ -1,7 +0,0 @@
-class Operacion:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
-    
-    def mostrar_resultado(self, resul):
-        print("el resultado es :", resul)
