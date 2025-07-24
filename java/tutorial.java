a7265c0c7432a4c0466e31f4c2dff5427faf3ed3
Jorge González <98619897+JorchZombie@users.noreply.github.com>
2025-07-23 20:24:40 -0600

    Delete java/tutorial.java

--- java/tutorial.java
+++
@@ -1,8 +0,0 @@
-class tutorial{
-    public static void main(String[] args) {
-      System.out.println("hola mundo");
-        for (int i=0;i<5;i++){
-          System.out.println("esto tiene que imprimirse 4 veces");
-       }
-    } 
-}
